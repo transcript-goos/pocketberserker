9c2d7179ca527e9dc5c2e084f884dd51e2959dd8
pocketberserker <pocketberserker@gmail.com>
2012-10-05 04:07:10 +0900

    スナイパーがオークションに接続するように変更

--- goos-scala/src/main/scala/auctionsniper/Main.scala
+++ goos-scala/src/main/scala/auctionsniper/Main.scala
@@ -1,15 +1,41 @@
 package auctionsniper
 
 import javax.swing.SwingUtilities
-import org.jivesoftware.smack.{Chat}
+import org.jivesoftware.smack.{Chat, MessageListener, XMPPConnection}
+import org.jivesoftware.smack.packet.Message
 
 object Main {
 
   val STATUS_JOINING = "Joining"
   val STATUS_LOST = "Lost"
+  private val ARG_HOSTNAME = 0
+  private val ARG_USERNAME = 1
+  private val ARG_PASSWORD = 2
+  private val ARG_ITEM_ID  = 3
+  private val AUCTION_RESOURCE = "Auction"
+  private val ITEM_ID_AS_LOGIN = "auction-%s"
+  private val AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE
 
   def main(args: Array[String]) {
     val main = new Main()
+    val connection = connectTo(args(ARG_HOSTNAME), args(ARG_USERNAME), args(ARG_PASSWORD))
+    val chat = connection.getChatManager().createChat(auctionId(args(ARG_ITEM_ID), connection),
+      new MessageListener() {
+        def processMessage(aChat: Chat, message: Message) {
+        }
+      })
+    chat.sendMessage(new Message())
+  }
+
+  private def connectTo(hostname: String, username: String, password: String) = {
+    val connection = new XMPPConnection(hostname)
+    connection.connect()
+    connection.login(username, password, AUCTION_RESOURCE)
+    connection
+  }
+
+  private def auctionId(itemId: String, connection: XMPPConnection) = {
+    AUCTION_ID_FORMAT.format(itemId, connection.getServiceName)
   }
 }
 
@@ -27,3 +53,4 @@ class Main {
     })
   }
 }
+
