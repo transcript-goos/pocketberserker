4e9c277703df35a2199a5747533b0306e0f4f081
pocketberserker <pocketberserker@gmail.com>
2012-10-05 03:32:37 +0900

    AuctionSniperDriverの実装

---
+++ goos-scala/src/test/scala/end2end/auctionsniper/AuctionSniperDriver.scala
@@ -0,0 +1,20 @@
+package end2end.auctionsniper
+
+import org.hamcrest.CoreMatchers._
+
+import com.objogate.wl.swing.driver.{ComponentDriver, JLabelDriver, JFrameDriver}
+import com.objogate.wl.swing.gesture.GesturePerformer
+import com.objogate.wl.swing.AWTEventQueueProber
+import auctionsniper.MainWindow
+
+class AuctionSniperDriver(val timeoutMillis: Int)
+  extends JFrameDriver(new GesturePerformer(),
+    JFrameDriver.topLevelFrame(
+      ComponentDriver.named(MainWindow.MAIN_WINDOW_NAME),
+      ComponentDriver.showingOnScreen()),
+    new AWTEventQueueProber(timeoutMillis, 100)) {
+
+  def showsSniperStatus(statusText: String) {
+    new JLabelDriver(this, ComponentDriver.named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(statusText))
+  }
+}
