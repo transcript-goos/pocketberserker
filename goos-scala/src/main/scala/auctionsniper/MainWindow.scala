bf44b678de6d3565cfa4a398ff12378b6b048c00
pocketberserker <pocketberserker@gmail.com>
2012-10-05 03:57:31 +0900

    スナイパーの状態を表示するためラベルの表示を追加

--- goos-scala/src/main/scala/auctionsniper/MainWindow.scala
+++ goos-scala/src/main/scala/auctionsniper/MainWindow.scala
@@ -1,18 +1,31 @@
 package auctionsniper
 
-import javax.swing.{JFrame}
+import javax.swing.{JLabel, JFrame}
+import javax.swing.border.LineBorder
+import java.awt.Color
 
 object MainWindow {
   val MAIN_WINDOW_NAME = "Auction Sniper Main"
   val SNIPER_STATUS_NAME = "sniper status"
+
+  private def createLabel(initialText: String) = {
+    val result = new JLabel(initialText)
+    result.setName(SNIPER_STATUS_NAME)
+    result.setBorder(new LineBorder(Color.BLACK))
+    result
+  }
 }
 
 class MainWindow extends JFrame("Auction Sniper") {
 
+  import Main._
   import MainWindow._
 
+  private val sniperStatus = createLabel(STATUS_JOINING)
+
   setName(MAIN_WINDOW_NAME)
+  add(sniperStatus)
+  pack()
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
   setVisible(true)
 }
-
