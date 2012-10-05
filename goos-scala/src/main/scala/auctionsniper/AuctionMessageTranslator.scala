ad1effb745134a92bd86294c58089c467fa17de9
pocketberserker <pocketberserker@gmail.com>
2012-10-05 18:51:26 +0900

    Translatorの構造化

--- goos-scala/src/main/scala/auctionsniper/AuctionMessageTranslator.scala
+++ goos-scala/src/main/scala/auctionsniper/AuctionMessageTranslator.scala
@@ -6,20 +6,41 @@ import org.jivesoftware.smack.packet.Message
 class AuctionMessageTranslator(private val listener: AuctionEventListener) extends MessageListener {
 
   def processMessage(chat: Chat, message: Message) {
-    val event = unpackEventFrom(message)
-    event("Event") match {
+    val event = AuctionEvent.from(message.getBody)
+    event.eventType match {
       case "CLOSE" => listener.auctionClosed()
       case "PRICE" =>
-        listener.currentPrice(event("CurrentPrice").toInt, event("Increment").toInt)
+        listener.currentPrice(event.currentPrice, event.increment)
       case _ => ()
     }
   }
 
-  private def unpackEventFrom(message: Message) = {
-    message.getBody.split(";").foldLeft(Map.empty[String, String]){
-      (event, element) =>
-        val pair = element.split(":")
-        event + (pair(0).trim -> pair(1).trim)
+  private class AuctionEvent {
+    val fields = new collection.mutable.HashMap[String, String]()
+
+    def eventType = get("Event")
+    def currentPrice = get("CurrentPrice").toInt
+    def increment = get("Increment").toInt
+
+    private def get(fieldName: String) = {
+      val value = fields(fieldName)
+      if(value == null) throw new Exception("Missing value for " + fieldName)
+      value
+    }
+
+    private def addField(field: String) {
+      val pair = field.split(":")
+      fields += (pair(0).trim -> pair(1).trim)
+    }
+  }
+
+  private object AuctionEvent {
+    def fieldIn(messageBody: String) = messageBody.split(";")
+    def from(messageBody: String) = {
+      val event = new AuctionEvent()
+      for (field <- fieldIn(messageBody))
+        event.addField(field)
+      event
     }
   }
 }
