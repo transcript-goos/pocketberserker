9ba4e41b6e6e6c4b1a25911fe66532a7146406fb
pocketberserker <pocketberserker@gmail.com>
2012-10-06 21:07:51 +0900

    TableModelに詳細を表示できるように実装

--- goos-scala/src/main/scala/auctionsniper/ui/SnipersTableModel.scala
+++ goos-scala/src/main/scala/auctionsniper/ui/SnipersTableModel.scala
@@ -1,16 +1,38 @@
 package auctionsniper.ui
 
 import javax.swing.table.AbstractTableModel
+import auctionsniper.SniperState
+import auctionsniper.ui.Column.{SNIPER_STATUS, LAST_BID, LAST_PRICE, ITEM_IDENTIFIER}
+
+object SnipersTableModel {
+  private val STARTING_UP = SniperState("", 0, 0)
+}
 
 class SnipersTableModel extends AbstractTableModel {
 
+  import SnipersTableModel._
+
   private var statusText = MainWindow.STATUS_JOINING
+  private var sniperState = STARTING_UP
 
   def getRowCount = 1
-  def getColumnCount = 1
-  def getValueAt(rowIndex: Int, columnIndex: Int) = statusText
+  def getColumnCount = Column.values.length
+  def getValueAt(rowIndex: Int, columnIndex: Int) = {
+    Column.at(columnIndex) match {
+      case ITEM_IDENTIFIER => sniperState.itemId
+      case LAST_PRICE => sniperState.lastPrice.asInstanceOf[Object]
+      case LAST_BID => sniperState.lastBid.asInstanceOf[Object]
+      case SNIPER_STATUS => statusText
+    }
+  }
 
   def setStatusText(newStatusText: String) {
+    statusText = newStatusText
+    fireTableRowsUpdated(0, 0)
+  }
+
+  def sniperStatusChanged(newSniperState: SniperState, newStatusText: String) {
+    sniperState = newSniperState
     statusText = newStatusText
     fireTableRowsUpdated(0, 0)
   }
---
+++ goos-scala/src/test/scala/unit/auctionsniper/ui/SniperTableModelTest.scala
@@ -0,0 +1,46 @@
+package unit.auctionsniper.ui
+
+import org.specs2.mutable.{Before, Specification}
+import org.specs2.mock.Mockito
+import javax.swing.event.{TableModelEvent, TableModelListener}
+import auctionsniper.ui.{MainWindow, Column, SnipersTableModel}
+import org.specs2.specification.BeforeExample
+import auctionsniper.SniperState
+import org.hamcrest.Matchers
+
+class SniperTableModelTest extends Specification{
+
+  private val model = new SnipersTableModel()
+
+  trait mock extends Mockito with Before {
+    val listener = mock[TableModelListener]
+    def before = model.addTableModelListener(listener)
+  }
+
+  "SniperTableModel" should {
+    "has enough columns" in {
+      model.getColumnCount must_== Column.values.length
+    }
+
+    "sets sniper values in columns" in new mock {
+      model.sniperStatusChanged(SniperState("item id", 555, 666), MainWindow.STATUS_BIDDING)
+
+      there was one(listener).tableChanged(anArgThat(aRowChangedEvent))
+
+      assertColumnEquals(Column.ITEM_IDENTIFIER, "item id")
+      assertColumnEquals(Column.LAST_PRICE, 555)
+      assertColumnEquals(Column.LAST_BID, 666)
+      assertColumnEquals(Column.SNIPER_STATUS, MainWindow.STATUS_BIDDING)
+    }
+  }
+
+  private def assertColumnEquals(column: Column, expected: Any) {
+    val rowIndex = 0
+    val columnIndex = column.ordinal
+    model.getValueAt(rowIndex, columnIndex) must_== expected
+  }
+
+  def aRowChangedEvent =
+    Matchers.samePropertyValuesAs(new TableModelEvent(model, 0))
+}
+
