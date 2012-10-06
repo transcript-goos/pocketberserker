51c716345cea72cf2859d8ddaffadacc9dcb2d73
pocketberserker <pocketberserker@gmail.com>
2012-10-06 09:11:08 +0900

    最終入札者を判別するためPriceSource型を導入

--- goos-scala/src/main/scala/auctionsniper/AuctionEventListener.scala
+++ goos-scala/src/main/scala/auctionsniper/AuctionEventListener.scala
@@ -4,3 +4,8 @@ trait AuctionEventListener {
   def auctionClosed() : Unit
   def currentPrice(price: Int, increment: Int) : Unit
 }
+
+trait PriceSource {}
+case class FromSniper() extends PriceSource
+case class FromOtherBidder() extends PriceSource
+
