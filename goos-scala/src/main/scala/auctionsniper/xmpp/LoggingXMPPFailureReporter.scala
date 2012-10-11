b00302da0db1db06a70b93bc7eddbea373f6a532
pocketberserker <pocketberserker@gmail.com>
2012-10-11 11:53:34 +0900

    cannotTranslateMessageを呼び出した際にloggerに通知するように実装

--- goos-scala/src/main/scala/auctionsniper/xmpp/LoggingXMPPFailureReporter.scala
+++ goos-scala/src/main/scala/auctionsniper/xmpp/LoggingXMPPFailureReporter.scala
@@ -3,5 +3,10 @@ package auctionsniper.xmpp
 import java.util.logging.Logger
 
 class LoggingXMPPFailureReporter(val logger: Logger) extends XMPPFailureReporter {
-  def cannotTranslateMessage(auctionId: String, failedMessage: String, exception: Exception) {}
+  def cannotTranslateMessage(auctionId: String, failedMessage: String, exception: Exception) {
+    logger.severe("<" + auctionId + "> "
+      + "Could not translate message \"" + failedMessage + "\" "
+      + "because \"" + exception + "\"")
+  }
 }
+
